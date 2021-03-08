eb5aaec136f9f1edc8f59474d189eef79d35d68e
achemiranda <timocasen78@hotmail.com>
2021-03-08 21:18:10 +0100

    paso 1 y paso 2

---
+++ src/main/java/Actividad2/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Actividad2;
+
+/**
+ *
+ * @author timoc
+ */
+public class Calculadora {
+    private Integer operador1;
+    private Integer operador2;
+    private Integer operacion;
+    
+    
+}
